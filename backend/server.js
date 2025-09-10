218eed6b7a8199c54f0b18b4bd15b3a9c47b2d0d
alexjhonN1 <164096252+alexjhonN1@users.noreply.github.com>
2025-09-10 03:36:54 -0500

    Estructura inicial del backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor backend corriendo 🚀");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
